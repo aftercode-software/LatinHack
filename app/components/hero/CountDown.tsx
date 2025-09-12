8a8d9235c3adaf2455ab4cd7f078fd238fddc92b
Geer68 <germhidalgo@gmail.com>
2025-09-12 12:21:02 -0300

    hero

---
+++ app/components/hero/CountDown.tsx
@@ -0,0 +1,7 @@
+export default function CountDown() {
+  return (
+    <section className="flex">
+      <aside></aside>
+    </section>
+  );
+}
--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,17 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
 import { useTranslations } from "next-intl";
 
 export default function Hero() {
   const t = useTranslations("Hero");
   return (
     <section className="h-[60vh] flex flex-col items-center justify-center gap-4">
+      <h1 className="uppercase">
+        {" "}
+        {t.rich("title", {
+          b: (chunks) => <b className="font-bold">{chunks}</b>,
+        })}
+      </h1>
+      <img src={"/logo.png"} alt="logo" />
       <span>{t("date")}</span>
-      <h1>LATIN HACK: {t("title")}</h1>
-      <div>
-        <p>{t("prizes.first")}</p>
-        <p>{t("prizes.second")} 🐐⚽️</p>
-        <p>{t("prizes.long")}</p>
+      <h2 className="text-4xl uppercase font-bold">{t("subtitle")}</h2>
+
+      <div className="items-center justify-center flex flex-col">
+        <section className="flex gap-4">
+          <p>{t("prizes.first")}</p>
+          <p>{t("prizes.second")} 🐐⚽️</p>
+        </section>
+        <p className="max-w-xl text-center">{t("prizes.long")}</p>
       </div>
-      <div className="space-x-4">
+      <div className="space-y-4 flex flex-col">
         <button>{t("primary-button")}</button>
         <button>{t("secondary-button")}</button>
       </div>
